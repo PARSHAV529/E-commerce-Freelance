3ab88b736a079a9c9eb9182b0e812730fe6dfb58
Parshav <parshav143@gmail.com>
2024-12-31 20:15:40 +0530

    c1

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,25 +1,21 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-
-import {thunk} from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import {thunk} from 'redux-thunk'; // Import thunk as default
+import { composeWithDevTools } from '@redux-devtools/extension'; // Use @redux-devtools/extension
 
 import { getProductDetailsReducer, getProductsReducer } from './reducers/productReducer';
 import { cartReducer } from './reducers/cartReducer';
-
 
 const reducer = combineReducers({
     getProducts: getProductsReducer,
     getProductDetails: getProductDetailsReducer,
     cart: cartReducer,
 });
-    
-const middleware = [thunk];
 
-
+const middleware = [thunk]
 
 const store = createStore(
     reducer,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
+    composeWithDevTools(applyMiddleware(...middleware)) // Use composeWithDevTools from @redux-devtools/extension
+);
 
 export default store;
